706d30303e67e324658a0680718ea725c7a529ad
Tony Corbett G0WFV <tonyc.uk@gmail.com>
2017-06-08 10:15:00 +0100

    Respond to MMDVM closing down

--- MMDVMNetwork.cpp
+++ MMDVMNetwork.cpp
@@ -265,7 +265,6 @@ void CMMDVMNetwork::close()
 
 	m_socket.write(buffer, HOMEBREW_DATA_PACKET_LENGTH, m_rptAddress, m_rptPort);
 	m_socket.close();
-
 }
 
 void CMMDVMNetwork::clock(unsigned int ms)
@@ -313,6 +312,8 @@ void CMMDVMNetwork::clock(unsigned int ms)
 			::memcpy(ack + 0U, "RPTACK", 6U);
 			::memcpy(ack + 6U, m_netId, 4U);
 			m_socket.write(ack, 10U, m_rptAddress, m_rptPort);
+		} else if (::memcmp(m_buffer, "RPTCL", 5U) == 0) {
+			::LogMessage("DMR, MMDVM is restarting");
 		} else if (::memcmp(m_buffer, "RPTC", 4U) == 0) {
 			m_configLen = length - 8U;
 			m_configData = new unsigned char[m_configLen];
