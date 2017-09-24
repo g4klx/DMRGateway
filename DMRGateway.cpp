3f03319e7fa36e3c810f608cc4e8863d951d9b18
Jonathan Naylor <naylorjs@yahoo.com>
2017-09-24 09:11:21 +0100

    Fix the XLX number mapping. Reported by Adrian VK4TUX.

--- DMRGateway.cpp
+++ DMRGateway.cpp
@@ -493,7 +493,7 @@ int CDMRGateway::run()
 					}
 				}
 			} else if (dstId > (m_xlxBase + 4000U) && dstId < (m_xlxBase + 5000U) && flco == FLCO_USER_USER && slotNo == m_xlxSlot) {
-				dstId += 4000U;
+				dstId -= 4000U;
 				dstId -= m_xlxBase;
 
 				if (dstId != m_xlxNumber)
