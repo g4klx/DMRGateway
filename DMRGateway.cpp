0dffbf9ddb0f44d157d2c09335b79fd6446458cb
Jonathan Naylor <naylorjs@yahoo.com>
2024-04-22 13:57:09 +0100

    Allow it to build on Windows again.

--- DMRGateway.cpp
+++ DMRGateway.cpp
@@ -70,7 +70,7 @@ static void sigHandler(int signum)
 const char* HEADER1 = "This software is for use on amateur radio networks only,";
 const char* HEADER2 = "it is to be used for educational purposes only. Its use on";
 const char* HEADER3 = "commercial networks is strictly prohibited.";
-const char* HEADER4 = "Copyright(C) 2017-2022 by Jonathan Naylor, G4KLX and others";
+const char* HEADER4 = "Copyright(C) 2017-2024 by Jonathan Naylor, G4KLX and others";
 
 int main(int argc, char** argv)
 {
